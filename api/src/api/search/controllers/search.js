3cd0eaea180b0e16b219c284a166b3c45f6a01a2
Dariwka <77488950+Dariwka@users.noreply.github.com>
2023-09-11 11:10:40 +0300

    deleted search controller, extra mobile styles

--- api/src/api/search/controllers/search.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * search controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::search.search');
--- api/src/api/search/services/search.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * search service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::search.search');
