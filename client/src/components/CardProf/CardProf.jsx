89b7e66b5b8205345e8ae056a78367e8a4cfa1f2
Dariwka <77488950+Dariwka@users.noreply.github.com>
2023-09-25 20:49:24 +0300

    cardprof styles

--- client/src/components/CardProf/CardProf.jsx
+++ client/src/components/CardProf/CardProf.jsx
@@ -1,36 +1,120 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./CardProf.scss";
 import styled from "styled-components";
 import { mobile } from "../../responsive";
 
+const LinkProf = styled(Link)`
+  text-decoration: none;
+  color: unset;
+`;
+const StyledCard = styled.div`
+  width: 275px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-bottom: 50px;
+  padding: 4px;
+  ${mobile({ width: "auto", marginBottom: "25px" })};
+`;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 400px;
+  overflow: hidden;
+  position: relative;
+
+  &:hover {
+    .secondImg {
+      z-index: 2;
+    }
+  }
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  background-color: white;
+  &.mainImg {
+    z-index: 1;
+  }
+  &.secondImg {
+  }
+`;
+const Stick = styled.span`
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  color: white;
+  padding: 3px;
+  z-index: 3;
+  font-weight: 500;
+  font-size: 14px;
+  &.new {
+    background-color: green;
+  }
+
+  &.popular {
+    background-color: blue;
+  }
+`;
+
+const Title = styled.h2`
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+const Prices = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const OldPrice = styled.h3`
+  font-size: 18px;
+  font-weight: 500;
+
+  &:first-child {
+    color: gray;
+    text-decoration: line-through;
+  }
+`;
+const NewPrice = styled.h3`
+  font-size: 18px;
+  font-weight: 500;
+
+  &:first-child {
+    color: gray;
+    text-decoration: line-through;
+  }
+`;
+
 const CardProf = ({ item }) => {
   return (
-    <Link className="link" to={`/professional/${item.id}`}>
-      <div className="cardProf">
-        <div className="image">
-          {item?.attributes.isNew && <span className="new">New </span>}
+    <LinkProf className="link" to={`/professional/${item.id}`}>
+      <StyledCard>
+        <ImageContainer>
+          {item?.attributes.isNew && <Stick className="new">New </Stick>}
           {item?.attributes.isPopular && (
-            <span className="popular">Popular </span>
+            <Stick className="popular">Popular </Stick>
           )}
-          <img
+          <Image
             src={item.attributes?.img?.data?.attributes?.url}
             alt=""
             className="mainImg"
           />
-          <img
+          <Image
             src={item.attributes?.img2?.data?.attributes?.url}
             alt=""
             className="secondImg"
           />
-        </div>
-        <h2>{item?.attributes.title}</h2>
-        <div className="prices">
-          <h3>€{item.oldPrice || item?.attributes.price + 20}</h3>
-          <h3>€{item?.attributes.price}</h3>
-        </div>
-      </div>
-    </Link>
+        </ImageContainer>
+        <Title>{item?.attributes.title}</Title>
+        <Prices>
+          <OldPrice>€{item.oldPrice || item?.attributes.price + 20}</OldPrice>
+          <NewPrice>€{item?.attributes.price}</NewPrice>
+        </Prices>
+      </StyledCard>
+    </LinkProf>
   );
 };
 
