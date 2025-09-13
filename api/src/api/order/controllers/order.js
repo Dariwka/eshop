1b375969cbe9411e12b00db20dc10c7bdffc0abe
Dariwka <77488950+Dariwka@users.noreply.github.com>
2025-09-13 21:11:00 +0300

    changes with payment method, removing klarna

--- api/src/api/order/controllers/order.js
+++ api/src/api/order/controllers/order.js
@@ -73,7 +73,7 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       ); // базовый payload
       //
       const sessionPayload = {
-        payment_method_types: ["card", "klarna"],
+        automatic_payment_methods: { enabled: true },
         mode: "payment",
         cancel_url: `${
           process.env.CLIENT_URL || "http://localhost:3000"
