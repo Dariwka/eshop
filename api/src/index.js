919195f8699144b93c0fbe259a8da5e98321bbfd
Dariwka <77488950+Dariwka@users.noreply.github.com>
2025-09-13 18:55:01 +0300

    return index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,26 +1,20 @@
 "use strict";
 
-const slugify = require("slugify");
-
 module.exports = {
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
   register(/*{ strapi }*/) {},
 
-  async bootstrap({ strapi }) {
-    strapi.log.info("[slug backfill] starting...");
-    const products = await strapi.db.query("api::product.product").findMany({
-      where: { slug: null },
-    });
-
-    for (const p of products) {
-      const newSlug = slugify(p.title, { lower: true, strict: true });
-      await strapi.db.query("api::product.product").update({
-        where: { id: p.id },
-        data: { slug: newSlug },
-      });
-    }
-
-    strapi.log.info(
-      `[slug backfill] done. Updated ${products.length} products`
-    );
-  },
+  /**
+   * An asynchronous bootstrap function that runs before
+   * your application gets started.
+   *
+   * This gives you an opportunity to set up your data model,
+   * run jobs, or perform some special logic.
+   */
+  bootstrap(/*{ strapi }*/) {},
 };
