d1d24213dc8eda92cfe0421ab1fb50f02dac2d38
Dariwka <77488950+Dariwka@users.noreply.github.com>
2025-09-13 18:23:14 +0300

    chore: align @strapi deps to 4.5.3 and remove import-export plugin

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -1,12 +1,11 @@
 module.exports = ({ env }) => ({
-  // ...
   upload: {
-    config: {
-      provider: "cloudinary",
+​    config: {
+      provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
         upload: {},
@@ -14,26 +13,22 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  // ...
-});
 
-// ./config/plugins.js//
-module.exports = ({ env }) => ({
   email: {
     config: {
-      provider: "nodemailer",
+      provider: 'nodemailer',
       providerOptions: {
-        host: env("SMTP_HOST", "smtp.hostinger.com"),
-        port: env.int("SMTP_PORT", 465), // 465 SSL (рекомендую). 587 -> secure:false
-        secure: true, // true для 465; если 587 — false
+        host: env('SMTP_HOST', 'smtp.hostinger.com'),
+        port: env.int('SMTP_PORT', 465), // 465 = SSL
+        secure: true, // для 465 = true, для 587 = false
         auth: {
-          user: env("SMTP_USER"), // ПОЛНЫЙ адрес ящика: info@kosmedik.eu
-          pass: env("SMTP_PASS"), // пароль от ящика в Hostinger (не Gmail!)
+          user: env('SMTP_USER'), // ПОЛНЫЙ адрес почты
+          pass: env('SMTP_PASS'), // Пароль почтового ящика
         },
       },
       settings: {
-        defaultFrom: env("SUPPORT_EMAIL", env("SMTP_USER")),
-        defaultReplyTo: env("SUPPORT_EMAIL", env("SMTP_USER")),
+        defaultFrom: env('SUPPORT_EMAIL', env('SMTP_USER')),
+        defaultReplyTo: env('SUPPORT_EMAIL', env('SMTP_USER')),
       },
     },
   },
